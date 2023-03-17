1c6d2fe4b50a84cc738aaad0f5a6bef37d9d1707
Avinash Yadav <avinash.guruka@gmail.com>
2023-03-17 17:07:48 +0530

    updating terraform-mutable-deployment

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,18 @@
-resource "null_resource" "null" {
-  count = ""
-  triggers = {
-    a = timestamp()
-  }
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
-      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
-      host     = ""
-    }
-
-    inline = [
-      "ansible-pull -U https://github.com/GurukaYadav/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${var.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${var.REDIS_ENDPOINT} -e RDS_ENDPOINT=${var.RDS_ENDPOINT}"
-    ]
-  }
-}
+#resource "null_resource" "null" {
+#  count = ""
+#  triggers = {
+#    a = timestamp()
+#  }
+#  provisioner "remote-exec" {
+#    connection {
+#      type     = "ssh"
+#      user     = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_USER"]
+#      password = jsondecode(data.aws_secretsmanager_secret_version.secret.secret_string)["SSH_PASS"]
+#      host     = ""
+#    }
+#
+#    inline = [
+#      "ansible-pull -U https://github.com/GurukaYadav/roboshop-ansible.git roboshop.yml -e HOST=localhost -e ROLE=${var.COMPONENT} -e ENV=${var.ENV} -e DOCDB_ENDPOINT=${var.DOCDB_ENDPOINT} -e REDIS_ENDPOINT=${var.REDIS_ENDPOINT} -e RDS_ENDPOINT=${var.RDS_ENDPOINT}"
+#    ]
+#  }
+#}
